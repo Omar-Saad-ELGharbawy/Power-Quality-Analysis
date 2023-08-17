34ad74fa0568c5718da5bd0501c6129dba2271ce
Omar Saad <omarsaadelgharbawy@gmail.com>
2023-08-17 09:13:12 +0300

    transient stft

--- Octave/Transient_Signals/STFT/rho_stft_transient.m
+++ Octave/Transient_Signals/STFT/rho_stft_transient.m
@@ -12,18 +12,19 @@ sampling_rate = 32000; % Sampling rate 32K Hz
 start_time=0.005; %t1 start duration
 
 # RHO Transient Signal
-%transient_amplitude = 7000;
-%transient_amplitude = transient_amplitude + 77;
-%transient_signal = generate_transient_rho(ac_time,transient_amplitude,start_time);
+#transient_amplitude = 2000;
+#transient_amplitude = transient_amplitude + 77;
+#transient_signal = generate_transient_rho(ac_time,transient_amplitude,start_time);
 
 # Sin Transient Signal
-transient_amplitude = 7000 *2.91;
+transient_amplitude = 4000 *2.91;
 transient_frequency = 6000; #transient frequency of 3.5K HZ
 transient_signal = generate_transient_sin(ac_time,transient_amplitude,transient_frequency,start_time);
 
-
 % AC power signal with 220 Volt, 50 Hz, Sampling Rate 32KHz, Contaning Transient at Start time
 power_transient = ac_signal + transient_signal;
+#power_transient =  ac_signal;
+
 
 ############################### Plots ##########################
 % figure
@@ -42,9 +43,9 @@ power_transient = ac_signal + transient_signal;
 ################ Short Time Fourier Transform (specgram) ###################
 window_time = 2; #  Window Time in ms (Each ms contains 32 points)
 
-step=ceil(0.4*sampling_rate/1000);    # one spectral slice every 1 ms
+#step=ceil(0.1*sampling_rate/1000);    # one spectral slice every 1 ms
 
-#step=ceil(31.25*sampling_rate/1000000);    # one spectral slice every 31.25 us
+step=ceil(31.25*sampling_rate/1000000);    # one spectral slice every 31.25 us
 
 window=ceil(window_time*sampling_rate/1000); # 1 ms data window
 nfft = 2^nextpow2(window);
@@ -61,12 +62,10 @@ nfft = 2^nextpow2(window);
 % colormap(jet);
 % colorbar;
 
-[tran_S, tran_f, tran_t] = specgram(transient_signal, 2^nextpow2(window), sampling_rate, window, window-step);
+[tran_S, tran_f, tran_t] = specgram(transient_signal, nfft, sampling_rate, window, window-step);
 tran_magnitude = abs(tran_S);
 tran_mag_mean = mean(tran_magnitude, 1);
 tran_t2 =tran_t +(window_time/1000);
-
-%find values in
 
 %%%%%%%%%%%%%%%%%%%%%%%%%% Sin Only STFT %%%%%%%%%%%%%%%%
 % figure
@@ -79,7 +78,7 @@ tran_t2 =tran_t +(window_time/1000);
 % colormap(jet);
 % colorbar;
 
-[sin_S, sin_f, sin_t] = specgram(ac_signal, 2^nextpow2(window), sampling_rate, window, window-step);
+[sin_S, sin_f, sin_t] = specgram(ac_signal, nfft, sampling_rate, window, window-step);
 sin_magnitude = abs(sin_S);
 sin_mag_mean = mean(sin_magnitude, 1);
 sin_t2 =sin_t +(window_time/1000);
@@ -94,33 +93,43 @@ sin_t2 =sin_t +(window_time/1000);
 % colormap(jet);
 % colorbar;
 
-[all_S, all_f, all_t] = specgram(power_transient, 2^nextpow2(window), sampling_rate, window, window-step);
+[all_S, all_f, all_t] = specgram(power_transient, nfft, sampling_rate, window, window-step);
 all_magnitude = abs(all_S);
 all_mag_mean = mean(all_magnitude, 1);
-all_t_shifted =all_t +(window_time/1000);
 
 sub_f = all_f(7:end);
-
-all_S_F3000 = all_S(7:end, :);
+#all_S_F3000 = all_S(7:end, :);
+all_S_F3000 = all_S(7:13, :);
 all_magnitude_F3000 = abs(all_S_F3000);
 all_mag_mean_F3000 = mean(all_magnitude_F3000, 1);
 
+
+#all_t = all_t ;
+all_t = all_t - 3.125e-05;
+#all_t_shifted =all_t +(window_time/1000);
+all_t_shifted =all_t +(window_time/1000)/2;
+
 % find index of transient elements in magnitude array
-#indices = find(all_mag_mean_F3000 > 300);
+indices = find_indices_greater_than(5,all_mag_mean_F3000) ;
 
-indices = find_indices_greater_than(300,all_mag_mean_F3000);
+time_occurs = all_t_shifted(indices);
+#tran_time_2 = floor(time_occurs);
+signal_index = (sampling_rate * time_occurs) +30;
 
-#print(indices);
-tran_time = all_t_shifted(indices);
-tran_time_2 = floar(tran_time);
-signal_index = (sampling_rate * tran_time) +1;
+% signal_index will be used for trignometric to find transient values
+
+#transient_values_2 = transient_signal(signal_index(1):signal_index(end));
+transient_values = power_transient(signal_index(1):signal_index(end));
+transient__time = ac_time(signal_index(1):signal_index(end));
+
+transient_values_only = transient_values -  220 * sin(2 * pi * 50 * transient__time );
+
+filtered_transient_values = get_elements_bigger_than(220,transient_values_only);
+
+peak_value = max(filtered_transient_values);
+
+length(filtered_transient_values);
+
+width = length(filtered_transient_values) * 3.125e-05;
 
 
-#indices(1);
-############################
-
-% [S2, f2, t2] = specgram(power_transient, 2^nextpow2(window), sampling_rate, window, window-step);
-% magnitude = abs(S2);
-% mag_mean = mean(magnitude, 1);
-% t3 =t2 +(window_time/1000)/   ;
-% t4 =t2 +(window_time/1000);
